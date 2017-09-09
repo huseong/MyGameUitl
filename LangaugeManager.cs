7866f5b1829a92f7fe1424f6c528b75dc2312c61
huseong <huseong@naver.com>
2017-09-09 09:44:40 +0900

    Add namespace

--- LangaugeManager.cs
+++ LangaugeManager.cs
@@ -7,65 +7,65 @@ using UnityEngine;
 /// You Need Singleton<T> and CSVParser
 /// </summary>
 /// This is enum of Pooling Object
-public enum Language {
-    English, Korean // you can add new language
+
+namespace GameUtilSD {
+    public enum Language {
+        English, Korean // you can add new language
+    }
+    public class LanguageManager : Singleton<LanguageManager> {
+        private Language _language;
+        private Language language {
+            get {
+                return _language;
+            }
+            set {
+                _language = value;
+                _currentFont = _fontDic[_language];
+            }
+        }
+        private Font _currentFont;
+        private Dictionary<Language, Dictionary<string, string>> _languageDic;
+        private Dictionary<Language, Font> _fontDic;
+        private Dictionary<Language, string> _languagePath;
+        private Dictionary<Language, string> _fontPath;
+        public delegate void onChangeLanguage(LanguageManager languageManager);
+        public onChangeLanguage onchangeLanguage;
+
+        protected void Awake() {
+            if (!isSingleton()) {
+                return;
+            }
+            _languageDic = new Dictionary<Language, Dictionary<string, string>>();
+            _fontDic = new Dictionary<Language, Font>();
+        }
+
+        private void Start() {
+            onchangeLanguage(instance);
+        }
+
+        private void setLanguagePath() {
+
+        }
+        /// <summary>
+        /// You have to add the Language's Path
+        /// </summary>
+        private void setFontPath() {
+
+        }
+
+        public void setLanguage(Language lauguage) {
+            this.language = lauguage;
+            onchangeLanguage(instance);
+        }
+
+        public string getText(string key) {
+            string temp;
+            if (_languageDic[_language].TryGetValue(key, out temp)) {
+                return temp;
+            }
+            Debug.LogWarning("No value linked this Key : " + key);
+            return "";
+        }
+    }
 }
-public class LanguageManager : Singleton<LanguageManager> {
-    private Language _language;
-    private Language language {
-        get {
-            return _language;
-        }
-        set {
-            _language = value;
-            _font = _fontDic[_language];
-        }
-    }
-    private Font _font;
-    private Dictionary<Language, Dictionary<string, string>> _languageDic;
-    private Dictionary<Language, Font> _fontDic;
-    private Dictionary<Language, string> _languagePath;
-    private Dictionary<Language, string> _fontPath;
-    public delegate void onChangeLanguage (LanguageManager languageManager);
-    public onChangeLanguage onchangeLanguage;
 
-    protected void Awake () {
-        if(!isSingleton()) {
-            return;
-        }
-        _languageDic = new Dictionary<Language, Dictionary<string, string>>();
-        _fontDic = new Dictionary<Language, Font>();
-        _languageList.Add(CSVReader.getDic("1. Language/01. Korean"));
-        _languageList.Add(CSVReader.getDic("1. Language/02. English"));
-        _fontArray = Resources.LoadAll<Font>("4. Fonts");
-        nowIndex = SecurePlayerPrefs.getData("Language", 0);
-    }
-
-    private void Start () {
-        onchangeLanguage(instance);
-    }
-
-    private void setLanguagePath() {
-        
-    }
-/// <summary>
-/// You have to add the Language's Path
-/// </summary>
-    private void setFontPath() {
-
-    }
-
-    public void setLanguage (Language lauguage) {
-        this.language = lauguage;
-        onchangeLanguage(instance);
-    }
-
-    public string getText (string key) {
-        string temp;
-        if (_languageDic[_language].TryGetValue(key, out temp)) {
-            return temp;
-        }
-        Debug.LogWarning('No value linked this Key : ' + key);
-        return "";
-    }
-}
--- ObjectPoolingManager.cs
+++ ObjectPoolingManager.cs
@@ -7,51 +7,53 @@ using System.Collections.Generic;
 /// You Need Singleton<T>
 /// </summary>
 /// This is enum of Pooling Object
-public enum PoolingObject {
+namespace GameUtilSD
+{
+    public enum PoolingObject {
 
-}
-
-public class ObjectPoolingManager : Singleton<ObjectPoolingManager> {
-    public Dictionary<PoolingObject, Queue<GameObject>> _pool;
-    private Dictionary<PoolingObject, GameObject> _gameObjectDic;
-
-    protected void Awake () {
-        if (!isSingleton()) {
-            return;
-        }
-        _pool = new Dictionary<PoolingObject, Queue<GameObject>>();
-        _gameObjectDic = new Dictionary<PoolingObject, GameObject>();
     }
+    public class ObjectPoolingManager : Singleton<ObjectPoolingManager> {
+        public Dictionary<PoolingObject, Queue<GameObject>> _pool;
+        private Dictionary<PoolingObject, GameObject> _gameObjectDic;
 
-    public void initObjects(PoolingObject type, GameObject gm, Transform parents, int poolCount) {
-        if(_pool.ContainsKey(type)) {
-            Debug.LogError(type.ToString() + "is already Inited");
-            return;
+        protected void Awake () {
+            if (!isSingleton()) {
+                return;
+            }
+            _pool = new Dictionary<PoolingObject, Queue<GameObject>>();
+            _gameObjectDic = new Dictionary<PoolingObject, GameObject>();
         }
-        _gameObjectDic.Add(type, gm);
-        Queue<GameObject> queue = new Queue<GameObject>();
-        for (int i=0; i<poolCount; i++) {
-            GameObject tempGm = Instantiate(gm);
-            tempGm.transform.parent = parents;
-            tempGm.SetActive(false);
-            tempGm.name = type.ToString() + '_' + i.ToString();
-            queue.Enqueue(gm);
-        }
-        _pool.Add(type, queue);
-    }
 
-    public GameObject getObject(PoolingObject type, bool isActive = false) {   
-        GameObject gm = _pool[type].Dequeue();
-        gm.SetActive(isActive);
-        return gm;
-    }
-
-    public void setObject(PoolingObject type, GameObject gm) {
-        if(!_pool.ContainsKey(type)) {
-            Debug.LogError(type.ToString() + "is not Inited");
-            return;
+        public void initObjects(PoolingObject type, GameObject gm, Transform parents, int poolCount) {
+            if(_pool.ContainsKey(type)) {
+                Debug.LogError(type.ToString() + "is already Inited");
+                return;
+            }
+            _gameObjectDic.Add(type, gm);
+            Queue<GameObject> queue = new Queue<GameObject>();
+            for (int i=0; i<poolCount; i++) {
+                GameObject tempGm = Instantiate(gm);
+                tempGm.transform.parent = parents;
+                tempGm.SetActive(false);
+                tempGm.name = type.ToString() + '_' + i.ToString();
+                queue.Enqueue(gm);
+            }
+            _pool.Add(type, queue);
         }
-        gm.SetActive(false);
-        _pool[type].Enqueue(gm);
-    }
+
+        public GameObject getObject(PoolingObject type, bool isActive = false) {   
+            GameObject gm = _pool[type].Dequeue();
+            gm.SetActive(isActive);
+            return gm;
+        }
+
+        public void setObject(PoolingObject type, GameObject gm) {
+            if(!_pool.ContainsKey(type)) {
+                Debug.LogError(type.ToString() + "is not Inited");
+                return;
+            }
+            gm.SetActive(false);
+            _pool[type].Enqueue(gm);
+        }
+    }  
 }
